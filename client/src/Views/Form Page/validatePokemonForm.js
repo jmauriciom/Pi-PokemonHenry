1d133e24a5bfb84f11f270db88d47d378591ba64
jmauriciom <skinet021@gmail.com>
2023-05-19 18:01:48 -0300

    validation terminado

--- client/src/Views/Form Page/validatePokemonForm.js
+++ client/src/Views/Form Page/validatePokemonForm.js
@@ -7,22 +7,22 @@ const validation = (value) => {
     if (!value.img) errors.img = "Need an image"
     if (!/\.(jpeg|jpg|gif|png|bmp|svg)$/i.test(value.img)) errors.img = "Enter Url Image"
 
-    if(!value.hp) errors.hp = "Need a hp"
+    if(!value.hp) errors.hp = "Need hp"
     if(value.hp < 0 || value.hp > 255) errors.hp = "Hp must be between 0 and 255"
 
-    if(!value.attack) errors.attack = "Need an attack"
+    if(!value.attack) errors.attack = "Need attack"
     if(value.attack < 0 || value.attack > 255) errors.attack = "Attack must be between 0 and 255"
 
-    if(!value.defense) errors.defense = "Need a defense"
+    if(!value.defense) errors.defense = "Need defense"
     if(value.defense < 0 || value.defense > 255) errors.defense = "Defense must be between 0 and 255"
 
-    if(!value.speed) errors.speed = "Need a speed"
+    if(!value.speed) errors.speed = "Need speed"
     if(value.speed < 0 || value.speed > 255) errors.speed = "Speed must be between 0 and 255"
 
-    if(!value.height) errors.height = "Need a height"
+    if(!value.height) errors.height = "Need height"
     if(value.height < 0 || value.height > 255) errors.height = "Height must be between 0 and 255"
 
-    if(!value.weight) errors.weight = "Need a weight"
+    if(!value.weight) errors.weight = "Need weight"
     if(value.weight < 0 || value.weight > 255) errors.weight = "Weight must be between 0 and 255"
 
     return errors
